20df64e1a698c1186442e42a511df87d889d10c7
tianzongw <tzwang.se@gmail.com>
2020-10-13 18:25:18 +0200

    codevide dataset

--- src/dividemix_cifar10.py
+++ src/dividemix_cifar10.py
@@ -4,7 +4,7 @@ from networks.network import get_model, train_model
 from networks.utils import augment, samplewise_loss, data2tensor, normlize_loss
 from sklearn.mixture import GaussianMixture
 
-threshold  = 0.01
+threshold  = 0.4
 BATCH_SIZE = 1024
 MAX_EPOCH = 10
 cifar10 = dataset("cifar10")
@@ -28,9 +28,6 @@ train_model(net1, cifar10_augmented,  BATCH_SIZE, MAX_EPOCH)
 
 net1_loss = samplewise_loss(net1, cifar10_augmented, all_metrics=False)
 net1_loss = normlize_loss(net1_loss)
-
-
-
 # model per-sample loss 
 gmm = GaussianMixture(n_components=2,max_iter=10,tol=1e-2,reg_covar=5e-4)
 gmm.fit(tf.reshape(net1_loss, (-1, 1)))
@@ -39,10 +36,10 @@ prob = prob[:,gmm.means_.argmin()]
 ind_labeled = prob > threshold
 ind_unlabeled = prob < threshold
 
-labeled_iamges, labeled_labels , unlabeled_images, unlabeled_labels= cifar10.co_divide(ind_labeled, ind_unlabeled) # not augmented
+labeled_images, labeled_labels , unlabeled_images, unlabeled_labels= cifar10.co_divide(ind_labeled, ind_unlabeled) # not augmented
 
-labeled_dataset = data2tensor(labeled_iamges, labeled_labels, BATCH_SIZE)
-unlabeled_dataset = data2tensor(unlabeled_iamges, unlabeled_labels, BATCH_SIZE)
+labeled_dataset = data2tensor(labeled_images, labeled_labels, BATCH_SIZE)
+unlabeled_dataset = data2tensor(unlabeled_images, unlabeled_labels, BATCH_SIZE)
 
 labeled_dataset_augmented = augment(labeled_dataset, BATCH_SIZE)
 unlabeled_dataset_augmented = augment(unlabeled_dataset, BATCH_SIZE)
